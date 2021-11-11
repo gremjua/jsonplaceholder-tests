1f27b6547d3e71d31f3a5bc15eecc4d4894bee85
Juan Gremes <jgremes@Juans-MacBook-Pro.local>
2021-11-11 20:33:16 -0300

    add users resource client

---
+++ src/resources/users/client.ts
@@ -0,0 +1,33 @@
+import { Identifiers, Resource } from 'resources/interfaces';
+import { request, ResponseType } from 'resources/request';
+
+import { User } from './types';
+
+const users: Resource<User> = {
+    delete: ({ id }: Identifiers): ResponseType =>
+        request({ method: 'DELETE', params: { id }, path: 'users/{id}' }).catch((error) => {
+            throw new Error(error);
+        }),
+    get: ({ id }: Identifiers): ResponseType =>
+        request({ method: 'GET', params: { id }, path: 'users/{id}' }).catch((error) => {
+            throw new Error(error);
+        }),
+    getBy: (values: Partial<User>): ResponseType =>
+        request({ method: 'GET', params: { query: values }, path: 'users' }).catch((error) => {
+            throw new Error(error);
+        }),
+    listAll: (): ResponseType =>
+        request({ method: 'GET', path: 'users' }).catch((error) => {
+            throw new Error(error);
+        }),
+    patch: (values: Identifiers & Partial<User>): ResponseType =>
+        request({ method: 'PATCH', params: { id: values.id, payload: values }, path: 'users/{id}' }).catch((error) => {
+            throw new Error(error);
+        }),
+    update: (user: User): ResponseType =>
+        request({ method: 'PUT', params: { id: user.id, payload: user }, path: 'users/{id}' }).catch((error) => {
+            throw new Error(error);
+        }),
+};
+
+export default users;

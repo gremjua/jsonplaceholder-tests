a30ae237f3a408a329b5655b9a44d8aaeb718019
Juan Gremes <jgremes@Juans-MacBook-Pro.local>
2021-11-12 00:47:16 -0300

    update location of reports

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     moduleDirectories: ['node_modules', 'src'],
-    reporters: [ "default", "jest-junit", "jest-html-reporters" ],
+    reporters: ['default', ['jest-junit', { outputDirectory: 'reports' }], ['jest-html-reporters', {publicPath: 'reports'}]],
     setupFiles: ['dotenv/config'],
 };
